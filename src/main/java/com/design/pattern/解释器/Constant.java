fdae5ec43fc7d98a6c8f6f45265b100baaee184d
huangyueran <492736173@qq.com>
2018-10-18 15:49:48 +0800

    解释器

---
+++ src/main/java/com/design/pattern/解释器/Constant.java
@@ -0,0 +1,39 @@
+package com.design.pattern.解释器;
+
+import com.design.pattern.解释器.expression.Expression;
+
+// 布尔常量
+public class Constant extends Expression {
+    
+    private boolean value;
+
+    public Constant(boolean value){
+        this.value = value;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        
+        if(obj != null && obj instanceof Constant){
+            return this.value == ((Constant)obj).value;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
+
+    @Override
+    public boolean interpret(Context ctx) {
+        
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return new Boolean(value).toString();
+    }
+    
+}
---
+++ src/main/java/com/design/pattern/解释器/expression/Not.java
@@ -0,0 +1,37 @@
+package com.design.pattern.解释器.expression;
+
+import com.design.pattern.解释器.Context;
+
+public class Not extends Expression {
+
+    private Expression exp;
+    
+    public Not(Expression exp){
+        this.exp = exp;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if(obj != null && obj instanceof Not)
+        {
+            return exp.equals(
+                    ((Not)obj).exp);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
+
+    @Override
+    public boolean interpret(Context ctx) {
+        return !exp.interpret(ctx);
+    }
+
+    @Override
+    public String toString() {
+        return "(Not " + exp.toString() + ")";
+    }
+
+}
